40a54479254af4dfc7133833b4aa4da89069ebdd
RyanMrts <ryanmartinssilva2006@gmail.com>
2023-04-02 13:41:04 -0300

    exercício pronto

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿int n1, n2, result;
+
+Console.WriteLine("Digite um número");
+n1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Digite outro número");
+n2 = Convert.ToInt32(Console.ReadLine());
+
+result = n1 + n2;
+
+Console.WriteLine($"{n1} + {n2} = {result}");
